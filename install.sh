12101c3174e742c58a35c502f0fb762fd44472fc
seavers <seavers@163.com>
2013-02-20 15:12:20 +0800

    add module concat from tengine-1.4.4

---
+++ install.sh
@@ -0,0 +1,3 @@
+./configure --prefix=/usr/local/nginx --add-module=src/third/concat
+make
+sudo make install
--- src/core/nginx.h
+++ src/core/nginx.h
@@ -11,7 +11,7 @@
 
 #define nginx_version      1003013
 #define NGINX_VERSION      "1.3.13"
-#define NGINX_VER          "nginx/" NGINX_VERSION
+#define NGINX_VER          "nginx-by-seavers/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
 #define NGX_OLDPID_EXT     ".oldbin"
